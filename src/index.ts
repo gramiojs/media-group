34adff7059b4aef032eb499374e6ac7ed422d83d
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-15 21:01:24 +0300

    feat: implement plugin

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,132 @@
-import { Plugin } from "gramio";
+import { type Bot, type ContextType, Plugin } from "gramio";
 
-export interface MediaGroupOptions {
-	// TODO:
-	some: 1;
-}
+// TODO: remove it. Slow types fixes for JSR
+export function mediaGroup(timeout = 150): Plugin<
+	{},
+	import("gramio").DeriveDefinitions & {
+		message:
+			| {
+					mediaGroup?: undefined;
+			  }
+			| {
+					mediaGroup:
+						| import("@gramio/contexts/dist/contexts/message").MessageContext<
+								Bot<{}, import("gramio").DeriveDefinitions>
+						  >[]
+						| undefined;
+			  };
+		edited_message:
+			| {
+					mediaGroup?: undefined;
+			  }
+			| {
+					mediaGroup:
+						| import("@gramio/contexts/dist/contexts/message").MessageContext<
+								Bot<{}, import("gramio").DeriveDefinitions>
+						  >[]
+						| undefined;
+			  };
+		channel_post:
+			| {
+					mediaGroup?: undefined;
+			  }
+			| {
+					mediaGroup:
+						| import("@gramio/contexts/dist/contexts/message").MessageContext<
+								Bot<{}, import("gramio").DeriveDefinitions>
+						  >[]
+						| undefined;
+			  };
+		edited_channel_post:
+			| {
+					mediaGroup?: undefined;
+			  }
+			| {
+					mediaGroup:
+						| import("@gramio/contexts/dist/contexts/message").MessageContext<
+								Bot<{}, import("gramio").DeriveDefinitions>
+						  >[]
+						| undefined;
+			  };
+		business_message:
+			| {
+					mediaGroup?: undefined;
+			  }
+			| {
+					mediaGroup:
+						| import("@gramio/contexts/dist/contexts/message").MessageContext<
+								Bot<{}, import("gramio").DeriveDefinitions>
+						  >[]
+						| undefined;
+			  };
+		edited_business_message:
+			| {
+					mediaGroup?: undefined;
+			  }
+			| {
+					mediaGroup:
+						| import("@gramio/contexts/dist/contexts/message").MessageContext<
+								Bot<{}, import("gramio").DeriveDefinitions>
+						  >[]
+						| undefined;
+			  };
+	}
+> {
+	const map = new Map<
+		string,
+		{
+			messages: ContextType<Bot, "message">[];
+			timeout: NodeJS.Timeout;
+		}
+	>();
 
-export function mediaGroup(options?: MediaGroupOptions) {
-	return new Plugin("@gramio/media-group");
+	return new Plugin("@gramio/media-group")
+		.on(
+			[
+				"message",
+				"channel_post",
+				"business_message",
+				"edited_message",
+				"edited_channel_post",
+				"edited_business_message",
+			],
+			(context, next) => {
+				if (!context.mediaGroupId) return next();
+				const key = `${context.chat.id}-${context.mediaGroupId}`;
+
+				const mediaGroupData = map.get(key);
+				if (!mediaGroupData) {
+					map.set(key, {
+						messages: [context],
+						timeout: setTimeout(next, timeout),
+					});
+				} else {
+					map.set(key, {
+						messages: [...mediaGroupData.messages, context],
+						timeout: mediaGroupData.timeout.refresh(),
+					});
+				}
+			},
+		)
+		.derive(
+			[
+				"message",
+				"channel_post",
+				"business_message",
+				"edited_message",
+				"edited_channel_post",
+				"edited_business_message",
+			],
+			(context) => {
+				if (!context.mediaGroupId) return {};
+				const key = `${context.chat.id}-${context.mediaGroupId}`;
+
+				const data = map.get(key);
+				if (data) map.delete(key);
+
+				return {
+					mediaGroup: data?.messages,
+				};
+			},
+		);
 }
